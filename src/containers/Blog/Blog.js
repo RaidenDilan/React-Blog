6075b5b8e327ff754139aaea48a948aa1203d223
RaidenDilan <raiden18@me.com>
2020-02-13 21:33:26 +0000

    Switching Between Pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -29,6 +30,10 @@ class Blog extends Component {
           path='/'
           exact
           component={ Posts }
+        />
+        <Route
+          path='/new-post'
+          component={ NewPost }
         />
       </div>
     );
